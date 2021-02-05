3242761be34c52fb7d08b029dc7a418fcfbe006e
Chris Patuzzo <chris@patuzzo.co.uk>
2021-02-05 18:58:09 +0000

    Initial commit
    Based on: https://smist08.wordpress.com/2021/01/08/apple-m1-assembly-language-hello-world/

---
+++ src/main.s
@@ -0,0 +1,14 @@
+.global _main
+.align 2
+
+_main:
+  mov X0, #1
+  adr X1, helloworld
+  mov X2, #13
+  mov X16, #4
+  svc 0
+  mov X0, #0
+  mov X16, #1
+  svc 0
+
+helloworld: .ascii  "Hello World!\n"
